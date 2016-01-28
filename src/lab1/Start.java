7a77feefa3572a56ed32c76bd8581e677e954b68
alancerio18 <alancerio18@alex>
2016-01-28 13:41:24 -0600

    Added instances

--- src/lab1/Start.java
+++ src/lab1/Start.java
@@ -10,18 +10,22 @@ package lab1;
  * @author alancerio18
  */
 public class Start {
+
     public static void main(String[] args) {
-        ProgrammingCourse h = new IntroToProgrammingCourse();
+        AdvancedJavaCourse advance = new AdvancedJavaCourse();
+        IntroJavaCourse introJava = new IntroJavaCourse();
+        IntroToProgrammingCourse Intro = new IntroToProgrammingCourse();
         
-        System.out.print("course name:" + h.getCourseName()+"\n" +
-                           "course number:" + h.getCourseNumber() + "\n" +
-                           "credits:"+ h.getCredits());
-        
-        
+        ProgrammingCourse h = new AdvancedJavaCourse();
+
+        System.out.print("course name:" + h.getCourseName() + "\n"
+                + "course number:" + h.getCourseNumber() + "\n"
+                + "credits:" + h.getCredits());
+
         /*
-        This is a good way to qucikly add in alot of methods to every class.
-        But then you have to worry about the dangers of inheritance.
-        You do get the added benefit of being able to use polymorphism.
-        */
+         This is a good way to qucikly add in alot of methods to every class.
+         But then you have to worry about the dangers of inheritance.
+         You do get the added benefit of being able to use polymorphism.
+         */
     }
 }
